db916d0140f52ba77aeaae39530d2fb7ef154e4b
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-03-21 22:22:53 +0500

    Custom + Third Party Modules

---
+++ Node4/Parent.js
@@ -0,0 +1,28 @@
+// const http = require('http');
+require('dotenv').config();
+const express = require('express')
+const app = express()
+
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hello, World!');
+// });
+
+//Custom Modules
+// const add = (a,b)=>{
+//     return a + b
+// }
+
+// server.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000');
+// });
+
+// module.exports = {add}
+
+//Third party Modules
+// app.get('/', (req,res)=>{
+//     res.send("Helo First Practice")
+// })
+
+app.listen(3000, ()=>{console.log("Server is Running at http://localhost:3000");})
