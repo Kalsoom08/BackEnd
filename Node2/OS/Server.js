a5265c5db5031f3b46838c9112c8048844bf3a96
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-03-28 09:23:45 +0500

    FS & OS modules

---
+++ Node2/OS/Server.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const os = require('os')
+
+const Server = http.createServer((req, res)=>{
+}).listen(3000, ()=>{
+    // console.log("Server is Running at http://localhost:3000");
+})
+
+//OS Modules
+// console.log(os.type());
+// console.log(os.platform());
+// console.log(os.arch());
+// console.log(os.release());
+// console.log(os.version());
+// console.log(os.uptime());
+// console.log(os.cpus());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+// console.log(os.userInfo());
+// console.log(os.hostname());
+// console.log(os.homedir());
+// console.log(os.tmpdir());
+
+
+
+
