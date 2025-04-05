d898fae87a35cb915cf3ad12930889ee1156dea7
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-04-05 17:00:23 +0500

    JWT(Authentivation, Authorization)

--- Express6/authMiddlware.js
+++ Express6/authMiddlware.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 
-const authMiddleware = (req, res, next) => {
-  const authHeader = req.header("Authorization")
-  if(!authHeader) return res.json({Message : "No Token Provided"})
+const authMiddleware = (req,res,next)=>{
+    const authHeader = req.header("Authorization")
+    if(!authHeader) return resizeBy.json({Message: "No Token Provided"})
+    
+    const token = authHeader.split(" ")[1]
 
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, SECRET_KEY)
-    req.user = decoded 
-    next()
-  } catch (error) {
-    res.json({Message : "Invalid Token"})
-  }
-}; 
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY)
+        req.user = decoded
+        next()
+    } catch (error) {
+        resizeBy.json({Message: "Invalid Token"})
+    }
+}
 
 module.exports = authMiddleware;
