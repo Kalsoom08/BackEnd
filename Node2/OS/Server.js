92921fa95006764c6f630e15a2518c42d90e01d4
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-05-31 16:13:20 +0500

    OS Module Practice

--- Node2/OS/Server.js
+++ Node2/OS/Server.js
@@ -1,17 +1,18 @@
 const http = require('http')
 const os = require('os')
+const PORT = 3000 
 
-const Server = http.createServer((req, res)=>{
-}).listen(3000, ()=>{
-    // console.log("Server is Running at http://localhost:3000");
+http.createServer((req, res)=>{
+}).listen(PORT, ()=>{
+   console.log("Runing on Port http://localhost:"+PORT);
 })
 
 //OS Modules
 // console.log(os.type());
 // console.log(os.platform());
 // console.log(os.arch());
-// console.log(os.release());
 // console.log(os.version());
+// console.log(os.release());
 // console.log(os.uptime());
 // console.log(os.cpus());
 // console.log(os.totalmem());
@@ -20,7 +21,3 @@ const Server = http.createServer((req, res)=>{
 // console.log(os.hostname());
 // console.log(os.homedir());
 // console.log(os.tmpdir());
-
-
-
-
