987b364501e9b67ffbd64a12ae49592f65629622
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-06-01 09:15:37 +0500

    Custom + 3rd party  module Practice

--- Node4/Parent.js
+++ Node4/Parent.js
@@ -1,27 +1,22 @@
-// const http = require('http');
+const http = require('http');
 const express = require('express')
 const app = express()
+const port = 3000
 
 
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end('Hello, World!');
-// });
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+}).listen(port, ()=>{console.log("Server is Running at http://localhost:3000")})
 
 //Custom Modules
-// const add = (a,b)=>{
-//     return a + b
-// }
-
-// server.listen(3000, () => {
-//     console.log('Server is running on http://localhost:3000');
-// });
-
-// module.exports = {add}
+exports.add = (a, b)=>{
+    return a + b;
+}
 
 //Third party Modules
 // app.get('/', (req,res)=>{
 //     res.send("Helo First Practice")
 // })
 
-app.listen(3000, ()=>{console.log("Server is Running at http://localhost:3000");})
+// app.listen(3000, ()=>{console.log("Server is Running at http://localhost:3000");})
