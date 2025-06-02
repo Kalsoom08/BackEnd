27d223f93db01cc1f5b7c6af266b79f3903923fc
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-06-02 08:52:09 +0500

    Pracitce on Application level Middleware

--- Express1/index.js
+++ Express1/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = 3000;
 
 // app.get("/", (req, res)=>{
 //     res.send("First Express Practice")
@@ -10,31 +11,29 @@ const app = express()
 //Application Middleware
 // Application-level middleware in Express.js is middleware that is directly bound to an app instance using app.use() or specific HTTP methods like app.get() and app.post(). It is used to process requests before they reach the route handler and can modify the req and res objects.
 
-
-
-// app.use((req, res, next) => { 
-//     console.log("Request received at:", new Date().toISOString()); 
-//     next(); 
-//     }); 
-
-//     app.get("/", (req, res)=>{
-//     res.send("First Express Practice")
+// app.use((req, res, next)=>{
+//     console.log("Date is " + new Date().toISOString());
+//     console.log("Passing to API end Point");
+//     next()
+// })
+// app.get("/", (req, res)=>{
+//     res.send("Application Middleware executed")
 // })
 
 
-//Authentication
-function authenticateUser(req, res, next) {
-    const token = 'secretKey';
+//Authentication(Application)
+// function authenticateUser(req, res, next) {
+//     const token = 'secretKey';
 
-    if (token === 'secretkey') {
-        next(); 
-    } else {
-        res.status(403).json({ error: 'Unauthorized access' }); 
-    }
-}
+//     if (token === 'secretkey') {
+//         next(); 
+//     } else {
+//         res.status(403).json({ error: 'Unauthorized access' }); 
+//     }
+// }
 
-const PORT = 3000;
-app.get('/dashboard', authenticateUser, (req, res) => {
-    res.send('Welcome to your dashboard!');
-});
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/dashboard`));
+
+// app.get('/dashboard', authenticateUser, (req, res) => {
+//     res.send('Welcome to your dashboard!');
+// });
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
