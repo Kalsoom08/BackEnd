60662d012b4d01aa696b60c42529f67ad440ab77
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-06-05 09:01:25 +0500

    Practice on Hash and Compare Password

--- Express7/index.js
+++ Express7/index.js
@@ -2,35 +2,26 @@ require('dotenv').config()
 const bcrypt = require('bcrypt')
 
 //Creating Hash Passowrd
-// const salt = 10;
+const salt = 10;
+const userPassword = process.env.Password
+bcrypt.hash(userPassword, salt, (err , hashPassword)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(hashPassword);
+    }
 
-// const userPassword = process.env.Password
-//  bcrypt.hash(userPassword, salt, (err,hash)=>{
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log(hash);
-//   }
-//  })
+bcrypt.compare(userPassword, hashPassword, (err, result)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Match Successfully', result);
+    }
+})
+})
 
-//Comparing
-// const PlainPassword = process.env.Password
-// const salt = 10
 
-// bcrypt.hash(PlainPassword, salt, (err, hashPassword)=>{
-//     if(err){
-//         console.log(err);
-//     }
 
-//     bcrypt.compare(PlainPassword,hashPassword, (err, result)=>{
-//     if(err) console.log(err);
-//     if (result){
-//         console.log("Matched");
-//     } else{
-//         console.log("Error");
-//     }
-// })
-// })
 
 
 
